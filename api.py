36e9ff2e09d83ccdb2cd630b3b265c6b2915d9dd
Michael-py <dairobidemichael@gmail.com>
2022-03-03 06:45:02 +0100

    Added GET request to API methods

--- api.py
+++ api.py
@@ -45,7 +45,7 @@ print ('-'*40)
 # Here the 'model_prediction()' function will be called when a POST request
 # is sent to our interface located at:
 # http:{Host-machine-ip-address}:5000/api_v0.1
-@app.route('/api_v0.1', methods=['POST'])
+@app.route('/api_v0.1', methods=['POST', 'GET'])
 def model_prediction():
     # We retrieve the data payload of the POST request
     data = request.get_json(force=True)
@@ -62,4 +62,4 @@ def model_prediction():
 # This will allow Flask to automatically restart itself everytime you
 # update your API code.
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
